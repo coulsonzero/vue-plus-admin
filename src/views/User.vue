576aae0c8188a0127881138515e32f6d833b9037
coulsonzero <coulson2021@163.com>
2025-09-18 16:51:22 +0800

    update

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,140 +1,165 @@
 <template>
-  <div>
-    <el-form :inline="true" :model="formInline" class="demo-form-inline">
-      <el-form-item label="Name">
-        <el-input v-model="formInline.user" placeholder="Name" clearable />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="handleQuery">Query</el-button>
-        <el-button type="primary" @click="handleAdd">Add</el-button>
-        <el-button type="danger" @click="handleDeleteList">多选删除</el-button>
-      </el-form-item>
-    </el-form>
-
-
+  <div class="table-box">
+    <div class="title">
+      <h2>CURD</h2>
+    </div>
+    <div class="query-box">
+      <el-input v-model="queryInput" style="width: 240px" placeholder="查询名称" @input="handleQueryName" />
+      <el-button type="primary" @click="handleAdd">新增</el-button>
+      <el-button type="danger" @click="handleDelList">删除多选</el-button>
+    </div>
     <!-- table -->
-    <el-table :data="tableData" style="width: 100%">
+    <el-table
+      border
+      :data="tableData"
+      style="width: 100%"
+      ref="multipleTableRef"
+      @selection-change="handleSelectionChange"
+    >
+      <!-- 多选框 -->
       <el-table-column type="selection" :selectable="selectable" width="55" />
-      <el-table-column fixed prop="date" label="Date" width="150" />
-      <el-table-column prop="name" label="Name" width="120" />
-      <el-table-column prop="state" label="State" width="120" />
-      <el-table-column prop="city" label="City" width="120" />
-      <el-table-column prop="address" label="Address" width="300" />
-      <el-table-column fixed="right" label="Operations" min-width="120">
-      <template #default="scope">
-        <el-button link type="primary" size="small" @click="handleEdit(scope.row)">Edit</el-button>
-        <el-button link type="danger" size="small" @click="handleDelete(scope.row)">Delete</el-button>
-      </template>
-    </el-table-column>
+      <el-table-column prop="name" label="姓名" width="120" />
+      <el-table-column prop="state" label="状态" width="120" />
+      <el-table-column prop="address" label="地址" width="600" />
+      <el-table-column fixed="right" label="操作" min-width="120">
+        <template #default="scope">
+          <el-button link type="primary" size="small" @click="handleEdit(scope.row)">编辑</el-button>
+          <el-button link type="danger" size="small" @click="handleDelete(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
   </el-table>
 
   <!-- dialog -->
-  <el-dialog v-model="dialogFormVisible" :title="dialogType === 'add' ? '新增' : '编辑'" width="500">
-  <el-form :model="tableForm">
-    <el-form-item label="Date" :label-width="80">
-      <el-input v-model="tableForm.date" autocomplete="off" />
-    </el-form-item>
-    <el-form-item label="Name" :label-width="80">
-      <el-input v-model="tableForm.name" autocomplete="off" />
-    </el-form-item>
-    <el-form-item label="State" :label-width="80">
-      <el-input v-model="tableForm.state" autocomplete="off" />
-    </el-form-item>
-    <el-form-item label="City" :label-width="80">
-      <el-input v-model="tableForm.city" autocomplete="off" />
-    </el-form-item>
-    <el-form-item label="Address" :label-width="80">
-      <el-input v-model="tableForm.address" autocomplete="off" />
-    </el-form-item>
-  </el-form>
-  <template #footer>
-    <div class="dialog-footer">
-      <el-button @click="dialogFormVisible = false">Cancel</el-button>
-      <el-button type="primary" @click="dialogFormVisible = false">
-        Confirm
-      </el-button>
-    </div>
-  </template>
-</el-dialog>
+  <el-dialog v-model="dialogFormVisible" width="500" :title="dialogType === 'add' ? '新增' : '编辑'">
+    <el-form :model="form">
+      <el-form-item label="姓名" :label-width="100">
+        <el-input v-model="form.name" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="状态" :label-width="100">
+        <el-select v-model="form.state" placeholder="状态">
+          <el-option label="在职" value="在职" />
+          <el-option label="离职" value="离职" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="地址" :label-width="100">
+        <el-input v-model="form.address" autocomplete="off" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button type="primary" @click="dialogConfirm">确认</el-button>
+      </div>
+    </template>
+  </el-dialog>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-let formInline = ref({
-  user: '',
-})
+import { ref, onMounted } from 'vue'
+import { getCustomers, createCustomer, updateCustomer, deleteCustomer, deleteCustomerList } from '@/api/customer'
+
+// date
+let queryInput = ref('')
+let tableData = ref([
+  // {
+  //   id: "1",
+  //   name: 'Tom1',
+  //   state: 'California',
+  //   address: 'No. 189, Grove St, Los Angeles',
+  // },
+  // {
+  //   id: "2",
+  //   name: 'Tom2',
+  //   state: 'California',
+  //   address: 'No. 189, Grove St, Los Angeles',
+  // },
+  // {
+  //   id: "3",
+  //   name: 'Tom3',
+  //   state: 'California',
+  //   address: 'No. 189, Grove St, Los Angeles',
+  // },
+])
+let multipleSelection = ref([])
 let dialogFormVisible = ref(false)
-let tableForm = ref({})
+let form = ref({})
 let dialogType = ref('add')
 
-const tableData = [
-{
-  id: 1,
-  date: '2016-05-03',
-  name: 'Tom',
-  state: 'California',
-  city: 'Los Angeles',
-  address: 'No. 189, Grove St, Los Angeles',
-  zip: 'CA 90036',
-  tag: 'Home',
-},
-{
-  id: 2,
-  date: '2016-05-02',
-  name: 'Tom',
-  state: 'California',
-  city: 'Los Angeles',
-  address: 'No. 189, Grove St, Los Angeles',
-  zip: 'CA 90036',
-  tag: 'Office',
-},
-{
-  id: 3,
-  date: '2016-05-04',
-  name: 'Tom',
-  state: 'California',
-  city: 'Los Angeles',
-  address: 'No. 189, Grove St, Los Angeles',
-  zip: 'CA 90036',
-  tag: 'Home',
-},
-{
-  id: 4,
-  date: '2016-05-01',
-  name: 'Tom',
-  state: 'California',
-  city: 'Los Angeles',
-  address: 'No. 189, Grove St, Los Angeles',
-  zip: 'CA 90036',
-  tag: 'Office',
-},
-]
 
-const handleClick = () => {
-  console.log('click')
+// methods
+// 刷新页面
+const refreshPage = () => {
+  location.reload();
+};
+
+const handleSelectionChange = (val) => {
+  // multipleSelection.value = val
+  multipleSelection.value = []
+  val.forEach(item => {
+    multipleSelection.value.push(item.ID)
+  })
 }
 
-
-
-function handleAdd() {
-  tableForm = {}
-  dialogType.value = 'add'
+const handleAdd = () => {
   dialogFormVisible.value = true
-
+  form.value = {}
 }
 
-function handleEdit(row) {
+const handleEdit = (row) => {
+  // 打开弹窗
+  dialogFormVisible.value = true
+  // title
   dialogType.value = 'edit'
-  dialogFormVisible.value = true
+  // 填充row数据
+  form.value = {...row}
+}
 
-  tableForm = {...row}
+const handleDelete = async (row) => {
+  await deleteCustomer(row.ID)
+  // 刷新页面
+  refreshPage()
+}
+
+const handleDelList = async () => {
+  await deleteCustomerList({ ids: multipleSelection.value })
+  refreshPage()
+}
+
+const handleQueryName = (val) => {
+  if (val.length > 0) {
+    tableData.value = tableData.value.filter(item => item.name.match(val))
+  } else {
+    refreshPage()
+  }
+}
+
+const dialogConfirm = async () => {
+  dialogFormVisible.value = false
+
+  if (dialogType.value === 'add') {
+    // 新增用户
+    await createCustomer(form.value)
+  } else if (dialogType.value === 'edit') {
+    // 编辑用户
+    let tem = {
+      "name": form.value.name,
+      "state": form.value.state,
+      "address": form.value.address,
+    }
+    await updateCustomer(form.value.ID, tem)
+  }
+  // 刷新页面
+  refreshPage()
 }
 
 
-function handleDelete(row) {
-  console.log(row.id)
-}
+onMounted(() => {
+  getCustomers().then(res => {
+    tableData.value = res.data.data
+  })
+})
+
 </script>
 
 <style>
